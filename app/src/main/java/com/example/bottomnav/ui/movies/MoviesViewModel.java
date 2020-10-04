ce503d4ebca7495842f5222df51fb12fe6f7fd18
PetitHerisson <eloodie.jc@gmail.com>
2020-10-04 15:30:26 +1300

    changed dashboard to movies

--- app/src/main/java/com/example/bottomnav/ui/movies/MoviesViewModel.java
+++ app/src/main/java/com/example/bottomnav/ui/movies/MoviesViewModel.java
@@ -1,14 +1,14 @@
-package com.example.bottomnav.ui.dashboard;
+package com.example.bottomnav.ui.movies;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class DashboardViewModel extends ViewModel {
+public class MoviesViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
 
-    public DashboardViewModel() {
+    public MoviesViewModel() {
         mText = new MutableLiveData<>();
         mText.setValue("This is dashboard fragment");
     }
