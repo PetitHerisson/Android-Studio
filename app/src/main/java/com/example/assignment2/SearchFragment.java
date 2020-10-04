e162cf41a843a098b9b191871836cc40800c3217
PetitHerisson <eloodie.jc@gmail.com>
2020-10-04 14:05:30 +1300

    created project

---
+++ app/src/main/java/com/example/assignment2/SearchFragment.java
@@ -0,0 +1,15 @@
+package com.example.assignment2;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class SearchFragment extends Fragment {
+    public View onCreatView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle saveInstanceState){
+        return inflater.inflate(R.layout.fragment_search, container, false);
+    }
+}
