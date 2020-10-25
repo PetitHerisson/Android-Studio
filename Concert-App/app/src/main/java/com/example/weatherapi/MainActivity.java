1e5033a439db3cc9a82e88cd1d7a05f6350c07c0
PetitHerisson <eloodie.jc@gmail.com>
2020-10-25 18:37:10 +1300

    added get user current location

--- Concert-App/app/src/main/java/com/example/weatherapi/MainActivity.java
+++ Concert-App/app/src/main/java/com/example/weatherapi/MainActivity.java
@@ -1,29 +1,46 @@
 package com.example.weatherapi;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.squareup.picasso.Picasso;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import static java.lang.Math.round;
+import java.util.List;
+import java.util.Locale;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LocationListener{
     private RequestQueue mQueue;
     private EditText editTextSearch;
     private ImageView imageViewSearch;
@@ -36,6 +53,8 @@ public class MainActivity extends AppCompatActivity {
     private TextView textViewLat;
     private TextView textViewLng;
     private static final String TAG = "Main Activity";
+    LocationManager locationManager;
+    FusedLocationProviderClient fusedLocationProviderClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,6 +68,20 @@ public class MainActivity extends AppCompatActivity {
         textViewDescription = findViewById(R.id.textViewDescription);
         textViewLat = findViewById(R.id.textViewlat);
         textViewLng = findViewById(R.id.textViewlng);
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{
+                    Manifest.permission.ACCESS_FINE_LOCATION
+            }, 100);
+        }
+        findViewById(R.id.btnMyLoc).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getLocation();
+            }
+        });
 
         editTextSearch = findViewById(R.id.editTextSearch);
         imageViewSearch = findViewById(R.id.imageViewSearch);
@@ -86,7 +119,7 @@ public class MainActivity extends AppCompatActivity {
                             String humidity = "Humidity: " + response.getJSONObject("main").getString("humidity");
                             String pressure = "Pressure: " + response.getJSONObject("main").getString("pressure");
                             String extra = wind + "\n" + humidity + "\n" + pressure;
-                            int temp = (int)(Double.parseDouble(response.getJSONObject("main").getString("temp")) - 273.15);
+                            int temp = (int) (Double.parseDouble(response.getJSONObject("main").getString("temp")) - 273.15);
                             Picasso.get().load(iconUrl).into(imageViewIcon);
                             Log.d(TAG, iconUrl);
 
@@ -99,7 +132,7 @@ public class MainActivity extends AppCompatActivity {
                             e.printStackTrace();
                         }
                     }
-                }, new Response.ErrorListener(){
+                }, new Response.ErrorListener() {
 
             @Override
             public void onErrorResponse(VolleyError error) {
@@ -113,5 +146,34 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, MapActivity.class);
         intent.putExtra(EXTRA_MESSAGE, coordinates);
         startActivity(intent);
+    }
+
+    @SuppressLint("MissingPermission")
+    private void getLocation() {
+        try{
+            locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                    5000, 5, this);
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+        try{
+            Geocoder geocoder= new Geocoder(this, Locale.getDefault());
+            List<Address> addresses = geocoder.getFromLocation(location.getLatitude(),
+                    location.getLongitude(), 1);
+            String lat = String.valueOf(location.getLatitude());
+            String lng = String.valueOf(location.getLongitude());
+            textViewLat.setText(lat);
+            textViewLng.setText(lng);
+            coordinates = lat + "/" + lng;
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
